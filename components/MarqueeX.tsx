7aeaaecd3ac69ec238f4ace0e8297a60f488a090
Aaron Cao <caoanlong@gmail.com>
2021-11-05 23:51:05 +0800

    Update MarqueeX.tsx

--- components/MarqueeX.tsx
+++ components/MarqueeX.tsx
@@ -7,7 +7,7 @@ import Notice from '../types/Notice'
 function MarqueeX() {
     const notices: Notice[] = useSelector((state: RootState) => state.config.notices)
     return (
-        <div className="flex items-center h-8 sm:h-11 bg-purple-500 text-white">
+        <div className="flex items-center h-8 sm:h-11 bg-pink-500 text-white">
             <div className="w-8 sm:w-11 flex justify-center">
                 <AiFillSound />
             </div>
